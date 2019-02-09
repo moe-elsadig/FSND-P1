ba4263b5baddc68d0a985bd7e4eac257e076114b
moe-elsadig <m.da7th@gmail.com>
2019-02-10 00:46:52 +0300

    question 3 sql completed

--- vagrant/newsdb.py
+++ vagrant/newsdb.py
@@ -13,6 +13,7 @@ print("************\n\nWhat are the most popular three articles?\n")
 # obtain the articles by the number of views
 news_c.execute("SELECT title, top_path.num FROM articles, (SELECT path, count(*) AS num FROM log WHERE log.status = '200 OK' GROUP BY path ORDER BY num desc) as top_path where top_path.path LIKE CONCAT('%',articles.slug,'%')")
 
+# Fetch the results of the query and print the top 3 items
 title_list = news_c.fetchall()
 for i in range(0,len(title_list[:3])):
     print ("\"" + title_list[i][0] + "\" - " + str(title_list[i][1]) + " views\n")
@@ -24,9 +25,19 @@ print("************\n\nWho are the most popular article authors?\n")
 # obtain the authors ordered by the sum of their article's views
 news_c.execute("SELECT authors.name, top_auth_id.path_sum FROM authors,(SELECT articles.author, sum(top_path.num) as path_sum FROM articles, (SELECT path, count(*) AS num FROM log WHERE log.status = '200 OK' GROUP BY path ORDER BY num desc) as top_path where top_path.path LIKE CONCAT('%',articles.slug,'%') GROUP BY articles.author ORDER BY path_sum desc) as top_auth_id WHERE authors.id = top_auth_id.author")
 
+#  Fetch the results of the query and print in descending order (already sorted from the query)
 author_views_list = news_c.fetchall()
 for i in range(len(author_views_list)):
     print ("\"" + author_views_list[i][0] + "\" - " + str(author_views_list[i][1]) + " views\n")
+
+
+# On which days did more than 1% of requests lead to errors?
+print ("************\n\nOn which days did more than 1% of requests lead to errors?")
+
+# Obtain the number of errors
+news_c.execute("SELECT day, perror from (SELECT DATE(time) AS day, count(*) as num, (sum(case when status='404 NOT FOUND' then 1 else 0 end)*100.00/count(status)) as perror FROM log GROUP BY day ORDER BY perror DESC) as query1 WHERE perror > 1.00")
+
+print(news_c.fetchall())
 
 # Exploring the tables!**********
 #
@@ -56,18 +67,18 @@ for i in range(len(author_views_list)):
 # news_c.execute("select time from articles")
 # print("\n  > time: " + str(news_c.fetchall()[1][0]))
 #
-print("\n\nFrom the log table:\n")
-news_c.execute("select id from log")
-print("\n  > id: " + str(news_c.fetchall()[1][0]))
-news_c.execute("select path from log")
-print("\n  > path: " + str(news_c.fetchall()[1][0]))
-news_c.execute("select ip from log")
-print("\n  > ip: " + str(news_c.fetchall()[1][0]))
-news_c.execute("select method from log")
-print("\n  > method: " + str(news_c.fetchall()[1][0]))
-news_c.execute("select status from log")
-print("\n  > status: " + str(news_c.fetchall()[1][0]))
-news_c.execute("select time from log")
-print("\n  > time: " + str(news_c.fetchall()[1][0]))
+# print("\n\nFrom the log table:\n")
+# news_c.execute("select id from log")
+# print("\n  > id: " + str(news_c.fetchall()[1][0]))
+# news_c.execute("select path from log")
+# print("\n  > path: " + str(news_c.fetchall()[1][0]))
+# news_c.execute("select ip from log")
+# print("\n  > ip: " + str(news_c.fetchall()[1][0]))
+# news_c.execute("select method from log")
+# print("\n  > method: " + str(news_c.fetchall()[1][0]))
+# news_c.execute("select status from log")
+# print("\n  > status: " + str(news_c.fetchall()[1][0]))
+# news_c.execute("select time from log")
+# print("\n  > time: " + str(news_c.fetchall()[1][0]))
 
 news_c.close()

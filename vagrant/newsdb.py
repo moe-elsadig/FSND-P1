998c879eeb336c0d25a3d65c4165060cdfcc22a2
moe-elsadig <m.da7th@gmail.com>
2019-02-11 18:19:18 +0300

    fixed formatting issues with the help of pycodestyle

--- vagrant/newsdb.py
+++ vagrant/newsdb.py
@@ -1,9 +1,8 @@
-#! /usr/bin/env python2.7
+#! /usr/bin/env python2
 
 # "Database code" for the DB Forum.
 import datetime
 import psycopg2
-import bleach
 
 news_db = psycopg2.connect("dbname=news")
 
@@ -12,33 +11,74 @@ news_c = news_db.cursor()
 # What are the most popular three articles?
 print("************\n\nWhat are the most popular three articles?\n")
 # obtain the articles by the number of views
-news_c.execute("SELECT title, top_path.num FROM articles, (SELECT path, count(*) AS num FROM log WHERE log.status = '200 OK' GROUP BY path ORDER BY num desc) as top_path where top_path.path LIKE CONCAT('%',articles.slug,'%')")
+news_c.execute("""
+    SELECT title, top_path.num
+    FROM articles,
+        (SELECT path, count(*) AS num
+            FROM log
+            WHERE log.status = '200 OK'
+            GROUP BY path
+            ORDER BY num desc) as top_path
+    WHERE top_path.path LIKE CONCAT('%', articles.slug, '%')""")
 
 # Fetch the results of the query and print the top 3 items
 title_list = news_c.fetchall()
-for i in range(0,len(title_list[:3])):
-    print ("\"" + title_list[i][0] + "\" - " + str(title_list[i][1]) + " views\n")
+for i in range(0, len(title_list[:3])):
+    print("\""
+          + title_list[i][0]
+          + "\" - " + str(title_list[i][1])
+          + " views\n")
 
 # Who are the most popular article authors?
-print("************\n\nWho are the most popular article authors?\n")
+print("""************\n\nWho are the most popular article authors?\n""")
 
 # obtain the authors ordered by the sum of their article's views
-news_c.execute("SELECT authors.name, top_auth_id.path_sum FROM authors,(SELECT articles.author, sum(top_path.num) as path_sum FROM articles, (SELECT path, count(*) AS num FROM log WHERE log.status = '200 OK' GROUP BY path ORDER BY num desc) as top_path where top_path.path LIKE CONCAT('%',articles.slug,'%') GROUP BY articles.author ORDER BY path_sum desc) as top_auth_id WHERE authors.id = top_auth_id.author")
+news_c.execute("""
+    SELECT authors.name, top_auth_id.path_sum
+    FROM authors,
+        (SELECT articles.author, sum(top_path.num) AS path_sum
+        FROM articles, (
+            SELECT path, count(*) AS num
+            FROM log
+            WHERE log.status = '200 OK'
+            GROUP BY path
+            ORDER BY num desc) as top_path
+        WHERE top_path.path LIKE CONCAT('%',articles.slug,'%')
+        GROUP BY articles.author
+        ORDER BY path_sum desc) as top_auth_id
+    WHERE authors.id = top_auth_id.author""")
 
-#  Fetch the results of the query and print in descending order (already sorted from the query)
+#  Fetch the results of the query and print in descending order
+# (already sorted from the query)
 author_views_list = news_c.fetchall()
 for i in range(len(author_views_list)):
-    print ("\"" + author_views_list[i][0] + "\" - " + str(author_views_list[i][1]) + " views\n")
+    print("\""
+          + author_views_list[i][0]
+          + "\" - "
+          + str(author_views_list[i][1])
+          + " views\n")
 
 # On which days did more than 1% of requests lead to errors?
-print ("************\n\nOn which days did more than 1% of requests lead to errors?\n")
+print("\n\nOn which days did more than 1% of requests lead to errors?\n")
 
 # Obtain the day where the percentage error is > 1%
-news_c.execute("SELECT day, perror from (SELECT DATE(time) AS day, count(*) as num, (sum(case when status='404 NOT FOUND' then 1 else 0 end)*100.00/count(status)) as perror FROM log GROUP BY day ORDER BY perror DESC) as query1 WHERE perror > 1.00")
+news_c.execute("""
+    SELECT day, perror
+    FROM (SELECT DATE(time) AS day,
+            count(*) as num,
+            (sum(CASE WHEN status='404 NOT FOUND'
+                THEN 1 ELSE 0 END)*100.00/count(status)) AS perror
+          FROM log
+          GROUP BY day
+          ORDER BY perror DESC) AS query1
+    WHERE perror > 1.00""")
 
 # print all the days where the error rate was more than 1%
 high_error_list = news_c.fetchall()
 for i in range(len(high_error_list)):
-    print ((high_error_list[i][0]).strftime('%B %d, %Y') + "  " +  str(round(high_error_list[i][1], 2)) + " %")
+    print((high_error_list[i][0]).strftime('%B %d, %Y')
+          + "  "
+          + str(round(high_error_list[i][1], 2))
+          + "%")
 
 news_c.close()
